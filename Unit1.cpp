c84a20394b0e373d13959cac4f6c9ebf9570d999
pusok <pusok-11@mail.ru>
2016-12-05 14:55:58 +0300

    2

--- Unit1.cpp
+++ Unit1.cpp
@@ -1,1 +1,14 @@
-
+                                    #include <conio.h>
+#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+using namespace std;
+int main()
+{
+    setlocale (LC_ALL, "RUS");
+    const int n=10;
+    int a[n], b[n];
+    cout <<"Vvedite chisla "<<"\t";
+    for(int i = 0;i<n;++i)
+        cin>>a[i];
+   
