92edbed9f021e9c98cab5fe5a0e33562a94cc128
pusok <pusok-11@mail.ru>
2016-12-05 14:56:19 +0300

    3

--- Unit1.cpp
+++ Unit1.cpp
@@ -1,4 +1,4 @@
-                                    #include <conio.h>
+#include <conio.h>
 #include <iostream>
 #include <time.h>
 #include <stdlib.h>
@@ -11,4 +11,16 @@ int main()
     cout <<"Vvedite chisla "<<"\t";
     for(int i = 0;i<n;++i)
         cin>>a[i];
-   
+    int j=0;
+     for(int i = n-1;i >= 0;--i)
+       {bool is = 0;
+           for(int k = i-1;k >= 0;--k)
+               if (a[i] == a[k]) is = 1;
+           if (!is) b[j++] = a[i];
+       }
+    cout<<"Ydalit povtor elementi\t";
+    for(int i = j;i >= 0;--i)
+        cout<<b[i]<<" ";
+  _getch();
+  return 0;
+}
